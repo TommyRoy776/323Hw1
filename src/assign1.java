5b525c44cc1c9aa83089acfe1b899f3f9f9e10b4
Roy776 <jj21861@gmail.com>
2022-03-05 21:13:44 -0500

    Full tests

--- src/assign1.java
+++ src/assign1.java
@@ -13,7 +13,7 @@ public class assign1 {
 	} 
 	
 
-	private static int[] insertionSort(int[] arr,AtomicInteger swap) {
+	private static int[] insertionSort(int[] arr,AtomicInteger swap,AtomicInteger compare) {
 		int[] myarr = arr.clone();
 		if(myarr.length < 2) {
 			return myarr;
@@ -24,6 +24,7 @@ public class assign1 {
 		   while(j>=0 && target < myarr[j]){
 			   myarr[j+1] = myarr[j];
 			   swap.incrementAndGet();
+			   compare.incrementAndGet();
 			   j--;
 		   }
 		   myarr[j+1] = target;
@@ -33,29 +34,29 @@ public class assign1 {
 		return myarr;
 	}
 	
-	private static int[] heapSort(int[] arr,AtomicInteger counter) {
+	private static int[] heapSort(int[] arr,AtomicInteger counter,AtomicInteger compare) {
 		int[] myarr = arr.clone();
-		heapSortCore(myarr,counter);
+		heapSortCore(myarr,counter,compare);
 	 
 		return myarr;
 	}
 	
-	private static void heapSortCore(int[] arr,AtomicInteger counter) {
+	private static void heapSortCore(int[] arr,AtomicInteger counter,AtomicInteger compare) {
 		int size = arr.length;
 		
 		for (int i = size / 2 - 1; i >=0; i--) {
-			 heapify(arr, size, i,counter);
+			 heapify(arr, size, i,counter,compare);
 		}
 		for(int i= size-1;i>0;i--) {
 			int max = arr[0];
 			arr[0] = arr[i];
 			arr[i] = max;
-			heapify(arr,i,0,counter);
+			heapify(arr,i,0,counter,compare);
 		}
 		
 	}
 	
-	private static void heapify(int[] arr,int size,int i,AtomicInteger counter){
+	private static void heapify(int[] arr,int size,int i,AtomicInteger counter,AtomicInteger compare){
 		if(2*i+1 > size-1) {
 			return;
 		}
@@ -73,33 +74,36 @@ public class assign1 {
             arr[i] = arr[max];
             arr[max] = swap;
             counter.incrementAndGet();
-            heapify(arr, size, max,counter);
+            heapify(arr, size, max,counter,compare);
         }
 	}
 	
-	private static int[] quickSort(int[] arr) {
+	private static int[] quickSort(int[] arr,AtomicInteger counter,AtomicInteger compare) {
 		int[] myarr = arr.clone();
-		quickSortCore(myarr,0,myarr.length-1);
+		quickSortCore(myarr,0,myarr.length-1,counter,compare);
 		return myarr;
 	}
 	
-	private static void quickSortCore(int[] arr,int start,int end) {
+	private static void quickSortCore(int[] arr,int start,int end,AtomicInteger counter,AtomicInteger compare) {
 		if(start > end) {
+			compare.incrementAndGet();
 			return; //sorted
 		}
 	    if(end > start) {
-	    	int part = partition(arr,start,end);
-	    	quickSortCore(arr,start,part-1);
-	    	quickSortCore(arr,part+1,end);
+	    	int part = partition(arr,start,end,counter,compare);
+	    	quickSortCore(arr,start,part-1,counter,compare);
+	    	quickSortCore(arr,part+1,end,counter,compare);
 	    }
 		 
 	}
 	
-	private static int partition(int[] arr,int start,int end) {
+	private static int partition(int[] arr,int start,int end,AtomicInteger counter,AtomicInteger compare) {
 		int pivot = arr[end];
 		int i = (start-1);
 		for(int j=start;j<=end-1;j++) {
 			if(arr[j] < pivot) {
+				compare.incrementAndGet();
+				counter.incrementAndGet();
 				int temp = arr[++i];
 				arr[i] = arr[j];
 				arr[j] = temp;
@@ -109,34 +113,38 @@ public class assign1 {
 		int temp = arr[i+1];
 		arr[i+1] = arr[end];
 		arr[end] = temp;
+		counter.incrementAndGet();
 		return (i+1);
 	}
 	
 	
 	
-	private static int[] mergeSort(int[] arr) {
+	private static int[] mergeSort(int[] arr,AtomicInteger counter,AtomicInteger compare,AtomicInteger tempA) {
 		int[] myarr = arr.clone();
-		mergeSortCore(myarr);
+		mergeSortCore(myarr,counter,compare,tempA);
 		return myarr;
 	}
 	
-	private static void mergeSortCore(int[] arr) {
+	private static void mergeSortCore(int[] arr,AtomicInteger counter,AtomicInteger compare,AtomicInteger tempA) {
 		int size = arr.length;
 		if(size<2) {return;}
 		int mid = size /2;
 		int[] left = Arrays.copyOfRange(arr, 0, mid);
 		int[] right = Arrays.copyOfRange(arr, mid, size);
-		mergeSortCore(left);
-		mergeSortCore(right);
-		merge(left,right,arr);
+		tempA.addAndGet(2);
+		mergeSortCore(left,counter,compare,tempA);
+		mergeSortCore(right,counter,compare,tempA);
+		merge(left,right,arr,counter,compare);
 		
 	} 
 	
-	private static void merge(int[] left,int[] right,int[] main) {
+	private static void merge(int[] left,int[] right,int[] main,AtomicInteger counter,AtomicInteger compare) {
 		int i=0;
 		int j =0;
 		while(i+j < main.length) {
+			compare.incrementAndGet();
 			if(i == right.length || (j < left.length && left[j] < right[i] )) {
+				compare.incrementAndGet();
 				main[i+j] = left[j];
 				j++;
 			}else {
@@ -149,19 +157,20 @@ public class assign1 {
 	private static void testInsert(int times,int[] arr) {
 		System.out.println("Measurment of insertion sort");
 		long start = System.currentTimeMillis();
-		AtomicInteger counter = new AtomicInteger(0);
+		AtomicInteger Swapcounter = new AtomicInteger(0);
+		AtomicInteger compare = new AtomicInteger(0);
 		for(int i=0;i<times;i++) {
 		
-			insertionSort(arr,counter);
+			insertionSort(arr,Swapcounter,compare);
 			if(i == 0) {
-				System.out.println("Number of swapping: "+counter);
+				System.out.println("Number of swapping: "+Swapcounter);
 			}
 			
 		}
 		long finish = System.currentTimeMillis();
 		long timeElapsed = finish - start;
 		System.out.println("InsertionSort Clock timeElapsed: "+timeElapsed);
-		System.out.println("Average: Clock time "+timeElapsed/times);
+		System.out.println("Average: Clock time "+timeElapsed/times+"\n");
 	} 
 	
 	
@@ -169,9 +178,10 @@ public class assign1 {
 		System.out.println("Measurment of Heap sort");
 		long start = System.currentTimeMillis();
 		AtomicInteger counter = new AtomicInteger(0);
+		AtomicInteger compare = new AtomicInteger(0);
 		for(int i=0;i<times;i++) {
 		
-			heapSort(arr,counter);
+			heapSort(arr,counter,compare);
 			if(i == 0) {
 				System.out.println("Number of swapping: "+counter);
 			}
@@ -180,8 +190,48 @@ public class assign1 {
 		long finish = System.currentTimeMillis();
 		long timeElapsed = finish - start;
 		System.out.println("HeapSort Clock timeElapsed: "+timeElapsed);
-		System.out.println("Average: Clock time "+timeElapsed/times);
+		System.out.println("Average: Clock time "+timeElapsed/times+"\n");
 	}
+	
+	private static void testQuickSort(int times,int[] arr) {
+		System.out.println("Measurment of Quicksort");
+		long start = System.currentTimeMillis();
+		AtomicInteger counter = new AtomicInteger(0);
+		AtomicInteger compare = new AtomicInteger(0);
+		for(int i=0;i<times;i++) {
+		
+			quickSort(arr,counter,compare);
+			if(i == 0) {
+				System.out.println("Number of swapping: "+counter);
+			}
+			
+		}
+		long finish = System.currentTimeMillis();
+		long timeElapsed = finish - start;
+		System.out.println("HeapSort Clock timeElapsed: "+timeElapsed);
+		System.out.println("Average: Clock time "+timeElapsed/times+"\n");
+	}
+	
+	private static void testMergeSort(int times,int[] arr) {
+		System.out.println("Measurment of MergeSort");
+		long start = System.currentTimeMillis();
+		AtomicInteger counter = new AtomicInteger(0);
+		AtomicInteger compare = new AtomicInteger(0);
+		AtomicInteger tempA = new AtomicInteger(0);
+		for(int i=0;i<times;i++) {
+		
+			mergeSort(arr,counter,compare, tempA);
+			if(i == 0) {
+				System.out.println("Number of temp arr: "+tempA);
+			}
+			
+		}
+		long finish = System.currentTimeMillis();
+		long timeElapsed = finish - start;
+		System.out.println("HeapSort Clock timeElapsed: "+timeElapsed);
+		System.out.println("Average: Clock time "+timeElapsed/times+"\n");
+	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -192,6 +242,9 @@ public class assign1 {
 		
 		testInsert(100,test);
 		testHeapSort(100,test);
+		testQuickSort(100,test);
+		testMergeSort(100,test);
+		
 		/*
 		System.out.println("MergeSort: "+Arrays.toString(mergeSort(test)));
 		System.out.println("QuickSort: "+Arrays.toString(quickSort(test)));
